942d9dc9dfda902acb413620a977b19d0e1e04ee
sreeranjan822 <sreeranjan822@gmail.com>
2025-01-30 11:15:55 +0530

    sms

---
+++ test.py
@@ -0,0 +1,9 @@
+# numbers going to add
+num1=input("first value:")
+num2=input("\nsecond value:")
+
+# adding num
+sum = float(num1)+ float(num2)
+
+#display
+print("sum of {0} and {1} is {2}".format(num1,num2,sum))
