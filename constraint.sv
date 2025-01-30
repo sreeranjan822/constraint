8f1c8108ee62ab38cbd81aefe3ec2635b05345ed
sreeranjan822 <sreeranjan822@gmail.com>
2025-01-30 11:13:10 +0530

    Update constraint.sv

--- constraint.sv
+++ constraint.sv
@@ -1,9 +1,19 @@
-class
+class patten;
+  rand int a[];
+  constraint c1{a.size;}
+  constraint c1{foreach (a[i])
+    a[i]=(10*i)+9;}
 
-function
-
-
-
-
-
+  function void post_randomize();
+    $display("the value of a is ",a);
+  endfunction
 endclass
+
+  module top;
+    patten h;
+    initial
+      begin
+        h=new();
+        h.randomize();
+      end
+  endmodule
